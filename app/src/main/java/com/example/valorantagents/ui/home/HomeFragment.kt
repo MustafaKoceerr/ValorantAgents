fb27ff032dbce56d3d94cdd756df37b75e51c21a
MustafaKoceerr <mustafakoceerr@hotmail.com>
2024-08-16 10:49:42 +0300

    agent data collect, bind with adapter.

--- app/src/main/java/com/example/valorantagents/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/valorantagents/ui/home/HomeFragment.kt
@@ -1,19 +1,30 @@
 package com.example.valorantagents.ui.home
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.lifecycle.ViewModel
+import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
-import com.example.valorantagents.R
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.valorantagents.data.network.IAgentApi
+import com.example.valorantagents.data.network.model.Resource
+import com.example.valorantagents.data.repository.NetworkRepository
 import com.example.valorantagents.databinding.FragmentHomeBinding
+import com.example.valorantagents.ui.adapter.AgentListAdapter
 import com.example.valorantagents.ui.base.BaseFragment
+import com.example.valorantagents.ui.viewmodel.HomeViewModel
+import com.example.valorantagents.ui.viewmodel.HomeViewModelFactory
+import com.example.valorantagents.util.visibleProgressBar
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 
 class HomeFragment : BaseFragment<FragmentHomeBinding>() {
-
+    private lateinit var viewModel: HomeViewModel
+    private lateinit var repository: NetworkRepository // Bu örnekte Repository'yi direkt aldığınızı varsayıyorum
 
     override fun getFragmentViewBinding(
         inflater: LayoutInflater,
@@ -28,14 +39,60 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>() {
         savedInstanceState: Bundle?
     ): View? {
         val view = super.onCreateView(inflater, container, savedInstanceState)
+        // Accessing ViewModel methods
+        viewModel = ViewModelProvider(
+            this,
+            HomeViewModelFactory(NetworkRepository(IAgentApi()))
+        ).get(HomeViewModel::class.java)
 
-        binding.txtFragHome.text = "DEGISTIRILDI BINDING CALISIYOR "
+        with(binding.mainRecyclerView) {
+            setHasFixedSize(true)
+            layoutManager = LinearLayoutManager(
+                requireContext(),
+                LinearLayoutManager.VERTICAL,
+                false
+            )
+        }
 
+        collectAgent()
+        viewModel.fechAgentFromNetwork()
         return view
     }
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
 
+
+    private fun collectAgent() {
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewModel.fetchAgent.collect { resource ->
+                when (resource) {
+                    is Resource.Loading -> {
+                            binding.progressbar.visibleProgressBar(true)
+                    }
+
+                    is Resource.Success -> {
+                        // Verileri işleyin
+                        val result = withContext(Dispatchers.IO) {
+                            viewModel.manipulateRawData(resource.value.data)
+                        }
+                            with(binding) {
+                                progressbar.visibleProgressBar(false)
+                                mainRecyclerView.adapter = AgentListAdapter(result)
+                        }
+                    }
+
+                    is Resource.Failure -> {
+                        // Hata durumunu göster
+                        showErrorMessage(HomeContants.ERROR_MESSAGE)
+
+                    }
+                }
+
+            }
+        }
+    }
+
+    private fun showErrorMessage(errorMessage: String) {
+            binding.progressbar.visibleProgressBar(false)
+            Toast.makeText(context, errorMessage, Toast.LENGTH_SHORT).show()
     }
 
 }
